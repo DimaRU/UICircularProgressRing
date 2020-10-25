0eaf918bdb0c54e09900853bb21efde749d5bc9a
Dmitriy Borovikov <dmitry@borovikov.name>
2020-10-25 16:01:32 +0300

    Set iOS deployment target to v9

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "UICircularProgressRing",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v9)],
     products: [
         .library(
             name: "UICircularProgressRing",
